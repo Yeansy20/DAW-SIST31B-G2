ab29320b167f80652d8ed502d6b1ebaad6d99575
Yeansy20 <50110267+Yeansy20@users.noreply.github.com>
2019-04-29 19:00:44 +0200

    Menu del proyecto
    para que enlacemos las demas paginas

---
+++ menu.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="estilos.css" type="text/css" media=screen>
+
+
+
+<ul class=menu>
+ <li class="dropdown">
+<a href="javascript:void(0)" class="dropbtn">INICIO</a>
+<div class="dropdown-content">
+	  <a href="?p=index.php">INICIO</a>
+             
+		</li>
+	
+</ul>                                                                                                                                                                      
